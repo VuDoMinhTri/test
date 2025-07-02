8a6fd09841fd7800bc70f4c17ddbc6b15e15af1a
VuDoMinhTri <vudominhtri@gmail.com>
2025-07-01 22:10:51 -0400

    Add files via upload

--- e.cpp
+++ e.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void palincheck(vector<int> arr, int n) {
-    for(int j = 0; j < n/2; j++) {
+    for(int j = 0; j < n; j++) {
         if(arr[j] != arr[n - j - 1]) {
             cout << "Your numbers suck";
             return;
@@ -11,12 +11,25 @@ void palincheck(vector<int> arr, int n) {
     cout << "Your number is worthy";
 }
 
+void knockoffPalincheck(string num) {
+    string reverseNum = num; reverse(reverseNum.begin(), reverseNum.end());
+    cout << reverseNum << endl;
+    if(num == reverseNum) {
+        cout << "Your number is worthy";
+    }
+    else {
+        cout << "Your numbers suck";
+    }
+}
+
 int main() {
-    string num; cin >> num;
+    int notstr; cin >> notstr;
+    string num = to_string(notstr);
     int n = num.size();
     vector<int> arr(n, 0);
     for(int i = 0; i < n; i++) {
         arr[i] == num.at(i) - '0';
     }
-    palincheck(arr, n);
+
+    knockoffPalincheck(num);
 }
